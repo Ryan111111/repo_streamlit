8292c5467550a2428c4f44b7bc71831b2390101c
Ryan <1075905766@qq.com>
2024-02-27 15:11:04 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,31 @@
 import streamlit as st
 import calendar
 
-# 获取当前年份和月份
-year = st.sidebar.number_input("Year", min_value=1900, max_value=2100, value=2024)
-month = st.sidebar.number_input("Month", min_value=1, max_value=12, value=1)
+def generate_month_calendar(year, month):
+    # 使用 calendar 模块生成指定年份和月份的日历
+    cal = calendar.monthcalendar(year, month)
+    
+    # 创建一个空的二维列表来存储日历表格
+    month_calendar = [[]]
+    
+    # 添加表头，即星期几的缩写
+    week_days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    month_calendar[0].extend(week_days)
+    
+    # 将日历数据添加到表格中
+    for week in cal:
+        month_calendar.append(week)
+    
+    return month_calendar
 
-# 生成日历
-cal = calendar.month(year, month)
+# 指定年份和月份
+year = 2024
+month = 2
 
-# 在 Streamlit 中显示日历
-st.write(f"## Calendar for {calendar.month_name[month]} {year}")
-st.write(cal)
+# 生成日历表格
+month_calendar = generate_month_calendar(year, month)
 
+# 在 Streamlit 应用中显示日历表格
+st.write("## Monthly Calendar")
+for week in month_calendar:
+    st.write(week)
